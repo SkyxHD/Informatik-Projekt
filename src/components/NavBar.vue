09ac0eefd125ed257fef6c2104c28eaa25794ae7
Philip <skyxhd6@gmail.com>
2023-01-20 11:16:24 +0100

    add navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-item">Home</div>
+    <div class="navbar-item">View posts</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+.navbar {
+  display: flex
+}
+.navbar-item {
+  margin-right: 20px
+}
+.navbar-item:last-of-type {
+  margin-right: 0
+}
+</style>
