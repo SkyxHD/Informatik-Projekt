f51ef33e7c0dc400fb9018258e284c3da791d69a
SkyxHD <120330370+SkyxHD@users.noreply.github.com>
2023-12-14 15:13:38 +0000

    Pending changes exported from your codespace

--- src/views/CreatePostView.vue
+++ src/views/CreatePostView.vue
@@ -7,6 +7,17 @@
 </template>
 
 <script>
+import axios from "axios"
+
 export default{
     name:"CreatePostView",
+
+    async createPost(post) {
+  try {
+    const response = await axios.post(`${this.url}/wp-json/wp/v2/posts`, post, { headers: this.headers });
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error while creating post: ${error}`);
+  }
+}
 }</script>
